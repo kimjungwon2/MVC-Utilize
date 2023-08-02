9bd1fa7e1cb2671d2ce13f8494abac153f659697
jungwon <mac20010@naver.com>
2023-08-03 01:39:05 +0900

    FEAT : formatter 스프링 적용하기

--- typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.convertor.IntegerToStringConverter;
 import hello.typeconverter.convertor.IpPortToStringConverter;
 import hello.typeconverter.convertor.StringToIntegerConverter;
 import hello.typeconverter.convertor.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
